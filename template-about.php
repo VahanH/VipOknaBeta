20dc1a8aa5d180d9d89d28db4a0ad3e44736bf9e
Smart <petrosyan.vahe@rambler.ru>
2014-12-21 15:11:18 +0400

    template-about fixes

--- template-about.php
+++ template-about.php
@@ -5,14 +5,14 @@ Template Name: About
 ?>
 
 <?php while (have_posts()) : the_post(); ?>
-    <?php get_template_part('templates/content', 'page'); ?>
+   <div class="margin-bottom"> <?php get_template_part('templates/content', 'page'); ?></div>
 <?php endwhile; ?>
 <div class="clear"></div>
 <?php $query = new WP_Query( array( 'category_name' => 'about-icons') ); $q=0;
 if ($query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); $q++;
-    if($q%2==1) echo '<div class="row info">'; ?>
+    if($q%2==1) echo '<div class="row">'; ?>
     <div class="col-md-6">
-        <div class="thumbnail">
+        <div class="thumbnail info">
             <div class="thumb"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
             <div class="caption">
                 <h3><? the_title() ?></h3>

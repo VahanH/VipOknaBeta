8b8ac01586cd1b79f313e3da4305ef24b57dff3a
VahanH <vahanius@gmail.com>
2014-12-21 20:38:34 +0400

    home page

--- template-home.php
+++ template-home.php
@@ -8,7 +8,16 @@ Template Name: Home
         <div class="general-slider"><img src="<?php bloginfo('template_url'); ?>/assets/img/retro_dra_69.jpg"></div>
     </div>
 <?php while (have_posts()) : the_post(); ?>
-    <?php get_template_part('templates/page', 'header'); ?>
+    <div>
+        <p class="about-title">О КОМПАНИИ</p>
+        <p class="rtecenter"><span>«VIP-ОКНА» — это производитель окон, который начал свое развитие в 1993
+                в Армении, и два года назад начали свою деятельность в городе Самара.</span>
+            <br>
+            <span> За это время мы успели заслужить доверие и уважение многих клиентов. Мы работаем
+                качественно, выполняя даже самые сложные заказы точно в срок.</span>
+
+        </p>
+    </div>
     <?php get_template_part('templates/content', 'page'); ?>
 
 <?php endwhile; ?>

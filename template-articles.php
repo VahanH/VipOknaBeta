314bfa53ccb87fa5fe9515c0ab8615bf79e34756
Smarter1 <vako3008@gmail.com>
2014-12-21 15:54:22 +0400

    articles

--- template-articles.php
+++ template-articles.php
@@ -8,8 +8,8 @@ Template Name: Articles
     <?php $category_name=str_replace("/", "",get_the_title() ); $query = new WP_Query(array('category_name' => $category_name ));
     if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
         <div class="row articlenews">
-            <div class="col-xs-4 thumbnail"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
-            <div class="col-xs-8 caption">
+            <div class="col-sm-4 col-xs-12 thumbnail"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
+            <div class="col-sm-8 col-xs-12 caption">
                 <h3><? the_title() ?></h3>
                 <p><? the_content() ?></p>
                 <button type="button" class="btn btn-default button-right">Подробнее</button>

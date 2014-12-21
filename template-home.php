fdf8b914b9f0ebabb43bad56634ec4d7b0a6f212
VahanH <vahanius@gmail.com>
2014-12-21 18:31:37 +0400

    home page

--- template-home.php
+++ template-home.php
@@ -3,8 +3,12 @@
 Template Name: Home
 */
 ?>
-
+    <div class="owl-carousel">
+        <div class="general-slider"><img class="" src="<?php bloginfo('template_url'); ?>/assets/img/slide1.jpg"></div>
+        <div class="general-slider"><img src="<?php bloginfo('template_url'); ?>/assets/img/retro_dra_69.jpg"></div>
+    </div>
 <?php while (have_posts()) : the_post(); ?>
     <?php get_template_part('templates/page', 'header'); ?>
     <?php get_template_part('templates/content', 'page'); ?>
+
 <?php endwhile; ?>

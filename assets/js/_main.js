dc08a8519091992b6e458caaeab7ba5c9ba66f69
VahanH <vahanius@gmail.com>
2014-12-21 19:02:38 +0400

    home page

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -29,7 +29,14 @@ var Roots = {
   home: {
     init: function() {
       // JavaScript to be fired on the home page
-      console.log("Home page");
+      $('.custom1').owlCarousel({
+        animateOut: 'slideOutDown',
+        animateIn: 'flipInX',
+        items:1,
+        margin:30,
+        stagePadding:30,
+        smartSpeed:450
+      });
     }
   },
   page_template_template_calculator_php: {

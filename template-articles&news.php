a224af0e37e45f1cfe830062ec7f77f0cd3c0255
VahanH <vahanius@gmail.com>
2014-12-21 23:19:17 +0400

    home page

--- template-articles&news.php
+++ template-articles&news.php
@@ -3,7 +3,22 @@
 Template Name: Articles & News
 */
 ?>
-
-<?php while (have_posts()) : the_post(); ?>
-    <?php get_template_part('templates/content', 'page'); ?>
-<?php endwhile; ?>
+    <div class="row info">
+        <?php $query = new WP_Query( array( 'category_name' => 'article&news') );
+        if ($query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();?>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <div class="thumb"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
+                    <div class="caption">
+                        <h3><? the_title() ?></h3>
+                        <p><? the_content() ?></p>
+                    </div>
+                </div>
+            </div>
+        <?
+        endwhile;
+        endif;
+        // Reset Query
+        //wp_reset_query();
+        ?>
+    </div>

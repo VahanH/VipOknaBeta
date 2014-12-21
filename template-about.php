f0114dc03a7f2620d322d2d365207ee15d2443ba
Smarter1 <vako3008@gmail.com>
2014-12-21 15:15:46 +0400

    home info

--- template-about.php
+++ template-about.php
@@ -8,22 +8,22 @@ Template Name: About
     <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
 <div class="clear"></div>
-<div class="row info">
-    <?php $query = new WP_Query( array( 'category_name' => 'about-icons') );
-    if ($query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();?>
-        <div class="col-md-6">
-            <div class="thumbnail">
-                <div class="thumb"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
-                <div class="caption">
-                    <h3><? the_title() ?></h3>
-                    <p><? the_content() ?></p>
-                </div>
+<?php $query = new WP_Query( array( 'category_name' => 'about-icons') ); $q=0;
+if ($query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); $q++;
+    if($q%2==1) echo '<div class="row info">'; ?>
+    <div class="col-md-6">
+        <div class="thumbnail">
+            <div class="thumb"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
+            <div class="caption">
+                <h3><? the_title() ?></h3>
+                <p><? the_content() ?></p>
             </div>
         </div>
-    <?
-    endwhile;
-    endif;
-    // Reset Query
-    //wp_reset_query();
-    ?>
-</div>
+    </div>
+<?
+    if($q%2==0) echo '</div>';
+endwhile;
+endif;
+// Reset Query
+//wp_reset_query();
+?>

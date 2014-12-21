3f8e2a9f0ff503362bfdbffbdeb5f753483ead76
VahanH <vahanius@gmail.com>
2014-12-21 22:06:06 +0400

    home page

--- template-home.php
+++ template-home.php
@@ -10,3 +10,25 @@ Template Name: Home
 <?php while (have_posts()) : the_post(); ?>
     <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
+
+<?php $query = new WP_Query( array( 'category_name' => 'Info') );
+if ($query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();?>
+<div class="row">
+    <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+            <img data-src="<? echo get_the_post_thumbnail($id, 'medium'); ?>" alt="...">
+            <div class="caption">
+                <h3><? the_title() ?></h3>
+                <p><? the_content() ?></p>
+                <p><a href="#" class="btn btn-primary" role="button">Кнопка</a> <a href="#" class="btn btn-default" role="button">Кнопка</a></p>
+            </div>
+        </div>
+    </div>
+</div>
+<?
+endwhile;
+endif;
+// Reset Query
+//wp_reset_query();
+
+?>

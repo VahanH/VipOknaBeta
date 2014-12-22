15a902069ac9abd2bb0e1a9aa1b3d428460a365e
VahanH <vahanius@gmail.com>
2014-12-22 10:48:05 +0400

    news template created

---
+++ template-news.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Template Name: News
+*/
+?>
+<div>
+    <?php $query = new WP_Query( array( 'category_name' => 'news') );
+    if ($query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();?>
+        <div class="row article">
+            <div class="col-xs-4 thumbnail"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
+            <div class="col-xs-8 caption">
+                <h3><? the_title() ?></h3>
+                <p><? the_content() ?></p>
+            </div>
+        </div>
+    <?
+    endwhile;
+    endif;
+    // Reset Query
+    //wp_reset_query();
+    ?>
+</div>

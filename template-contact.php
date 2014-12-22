ae260bb5e5d351132cf835235db304f751520c49
VahanH <vahanius@gmail.com>
2014-12-22 17:09:28 +0400

    contact page

---
+++ template-contact.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Template Name: Contact
+*/
+?>
+<div class="node-content">
+    <div class="field field-name-body field-type-text-with-summary field-label-hidden">
+        <div class="field-item even" property="content:encoded">
+            <script type="text/javascript" charset="utf-8"
+                    src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=wkZjX6NT_PZUqK5VW0VszGtlmslcsUui&amp;width=550&amp;height=300"></script>
+            <div class="address">
+                Наш адрес<br/>
+                ул.3 Линия д. 191 (Алма-атинская)
+            </div>
+            <div class="phone">
+                Наш телефон<br/>
+                271 51 11<br/>
+                8 (987)151 44 44
+            </div>
+            <div class="write_us">
+                НАПИШИТЕ НАМ
+            </div>
+        </div>
+    </div>
+</div>

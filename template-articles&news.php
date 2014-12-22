3646f301b92e9f4df852e9b7f50526e5743ed8c8
VahanH <vahanius@gmail.com>
2014-12-22 10:24:38 +0400

    home page

--- template-articles&news.php
+++ template-articles&news.php
@@ -3,22 +3,20 @@
 Template Name: Articles & News
 */
 ?>
-    <div class="row info">
-        <?php $query = new WP_Query( array( 'category_name' => 'article&news') );
-        if ($query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();?>
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <div class="thumb"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
-                    <div class="caption">
-                        <h3><? the_title() ?></h3>
-                        <p><? the_content() ?></p>
-                    </div>
+<div>
+    <?php $query = new WP_Query( array( 'category_name' => 'article&news') );
+    if ($query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();?>
+        <div class="row">
+                <div class="col-xs-4 thumb"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
+                <div class="col-xs-8 caption">
+                    <h3><? the_title() ?></h3>
+                    <p><? the_content() ?></p>
                 </div>
-            </div>
-        <?
-        endwhile;
-        endif;
-        // Reset Query
-        //wp_reset_query();
-        ?>
-    </div>
+        </div>
+    <?
+    endwhile;
+    endif;
+    // Reset Query
+    //wp_reset_query();
+    ?>
+</div>
--- template-products.php
+++ template-products.php
@@ -29,8 +29,8 @@ Template Name: Products
 $first="active in";
 if ($query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();?>
         <div id="<?=get_the_ID() ?>" role="tabpanel" class="tab-pane fade <?=$first;?>" aria-labelledby="<? the_title() ?>">
-            <? echo get_the_post_thumbnail($id,'medium');?>
-            <p><? the_content() ?></p>
+           <div class="col-xs-4"> <? echo get_the_post_thumbnail($id,'medium');?></div>
+            <div class="col-xs-8"><p><? the_content() ?></p></div>
         </div>
 <?
 $first="";

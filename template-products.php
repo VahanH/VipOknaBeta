0ae6e59128ee6dc54656e8f95c82b99a477ff298
VahanH <vahanius@gmail.com>
2014-12-22 15:26:36 +0400

    about template updated

--- template-products.php
+++ template-products.php
@@ -28,7 +28,7 @@ Template Name: Products
 <?php $query = new WP_Query( array( 'category_name' => 'products') );
 $first="active in";
 if ($query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();?>
-        <div id="<?=get_the_ID() ?>" role="tabpanel" class="tab-pane fade <?=$first;?> article" aria-labelledby="<? the_title() ?>">
+        <div id="<?=get_the_ID() ?>" role="tabpanel" class="product_img tab-pane fade <?=$first;?>" aria-labelledby="<? the_title() ?>">
            <div class="col-xs-4 thumbnail"> <? echo get_the_post_thumbnail($id,'medium');?></div>
             <div class="col-xs-8"><p><? the_content() ?></p></div>
         </div>
@@ -44,3 +44,22 @@ endif;
     </div>
 </div>
 
+<div class="row info">
+    <?php $query = new WP_Query( array( 'category_name' => 'Info') );
+    if ($query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();?>
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <div class="thumb"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
+                <div class="caption">
+                    <h3><? the_title() ?></h3>
+                    <p><? the_content() ?></p>
+                </div>
+            </div>
+        </div>
+    <?
+    endwhile;
+    endif;
+    // Reset Query
+    //wp_reset_query();
+    ?>
+</div>

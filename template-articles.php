6e396c78eb8bd9e39e55eaaaf774a547cd5a703b
VahanH <vahanius@gmail.com>
2014-12-22 12:57:58 +0400

    about template updated

--- template-articles.php
+++ template-articles.php
@@ -1,18 +1,19 @@
 <?php
+
 /*
 Template Name: Articles
 */
 ?>
 <div>
-    <?php $query = new WP_Query( array( 'category_name' => 'articles') );
-    if ($query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();?>
+    <?php $query = new WP_Query(array('category_name' => 'articles'));
+    if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
         <div class="row articlenews">
-                <div class="col-xs-4 thumbnail"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
-                <div class="col-xs-8 caption">
-                    <h3><? the_title() ?></h3>
-                    <p><? the_content() ?></p>
-                    <button type="button" class="btn btn-default">Подробнее</button>
-                </div>
+            <div class="col-xs-4 thumbnail"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
+            <div class="col-xs-8 caption">
+                <h3><? the_title() ?></h3>
+                <p><? the_content() ?></p>
+                <button type="button" class="btn btn-default">Подробнее</button>
+            </div>
         </div>
     <?
     endwhile;
@@ -21,3 +22,4 @@ Template Name: Articles
     //wp_reset_query();
     ?>
 </div>
+
--- templates/footer.php
+++ templates/footer.php
@@ -1,10 +1,19 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
+    <div class="navbar-footer">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+  <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
     <nav class="vip-footer-nav" role="navigation">
       <?php
       if (has_nav_menu('footer_navigation')) :
-        wp_nav_menu(array('theme_location' => 'footer_navigation', 'walker' => new Roots_Nav_Walker(), 'menu_class' => 'vip-nav'));
+        wp_nav_menu(array('theme_location' => 'footer_navigation', 'walker' => new Roots_Nav_Walker(), 'menu_class' => 'nav navbar-nav navbar-center'));
       endif;
       ?>
     </nav>

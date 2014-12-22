5e153a42fa4cf3b740b7b71cc36582efcc9ca265
VahanH <vahanius@gmail.com>
2014-12-22 11:12:56 +0400

    news template updated

--- template-articles&news.php
+++ template-articles&news.php
@@ -6,7 +6,7 @@ Template Name: Articles & News
 <div>
     <?php $query = new WP_Query( array( 'category_name' => 'article&news') );
     if ($query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();?>
-        <div class="row article">
+        <div class="row articlenews">
                 <div class="col-xs-4 thumbnail"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
                 <div class="col-xs-8 caption">
                     <h3><? the_title() ?></h3>

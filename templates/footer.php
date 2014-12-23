0689317de8ce3a5ab3ffdfaedc7307977e43afeb
Smart <petrosyan.vahe@rambler.ru>
2014-12-23 18:38:20 +0400

    обратная связь

--- templates/footer.php
+++ templates/footer.php
@@ -9,4 +9,20 @@
       ?>
     </nav>
   </div>
+
+
+
+
 </footer>
+<div>
+  <!-- Large modal -->
+  <button type="button" class="btn btn-danger call-order-button" data-toggle="modal" data-target=".bs-example-modal-lg">Обратная Связь</button>
+
+  <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <?php echo do_shortcode( '[contact-form-7 id="280" title="Контактная форма 1"]' ); ?>
+      </div>
+    </div>
+  </div>
+</div>

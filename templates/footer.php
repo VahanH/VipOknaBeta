d3888bcd19851a9efc1c99735a97575611a9fe75
VahanH <vahanius@gmail.com>
2014-12-23 18:33:42 +0400

    footer updated

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,4 @@
-<footer class="content-info" role="contentinfo">
+<footer class="content-info hidden-xs hidden-sm" role="contentinfo">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
     <nav class="vip-footer-nav" role="navigation">

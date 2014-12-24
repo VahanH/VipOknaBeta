bf0e8a13cd6f3537fadf79343ece8784f2230b13
Smart <petrosyan.vahe@rambler.ru>
2014-12-24 17:20:52 +0400

    contact-form-7 add in footer

--- templates/footer.php
+++ templates/footer.php
@@ -9,20 +9,24 @@
       ?>
     </nav>
   </div>
-
-
-
-
 </footer>
-<div>
-  <!-- Large modal -->
-  <button type="button" class="btn btn-danger call-order-button" data-toggle="modal" data-target=".bs-example-modal-lg">Обратная Связь</button>
 
-  <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-      <div class="modal-content">
+<!-- Button trigger modal -->
+<div class="btn btn-danger div-contact-button" data-toggle="modal" data-target="#myModal">
+  <p class="div-p-contact-button">Обратная связь</p>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Обратная связь</h4>
+      </div>
+      <div class="modal-body">
         <?php echo do_shortcode( '[contact-form-7 id="280" title="Контактная форма 1"]' ); ?>
       </div>
     </div>
   </div>
-</div>
+  <div>

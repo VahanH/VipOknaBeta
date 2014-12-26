ac10ecb7bd0f4ee4023450a5f58cf01ac7b47786
Smart <petrosyan.vahe@rambler.ru>
2014-12-26 19:09:10 +0400

    calc

--- template-calculator.php
+++ template-calculator.php
@@ -7,7 +7,7 @@ Template Name: Calculator
     <!-- Default panel contents -->
     <div class="panel-heading"><h4>1. Тип конструкции</h4></div>
     <div class="panel-body">
-        <div class="container">
+        <div>
                     <label>
                         <input type="radio" name="optionsRadios" id="optionsRadios2" value="option1" >
                        <span> Глухое окно</span>
@@ -62,5 +62,132 @@ Template Name: Calculator
                         <img src="<?php bloginfo('template_url'); ?>/assets/img/dver.png">
                     </label>
         </div>
+    </div>
+</div>
+<div>
+    <div class="col-xs-3">
+        <div class="panel panel-danger">
+               <!-- Default panel contents -->
+               <div class="panel-heading"><h4>2. Размер</h4></div>
+               <div class="panel-body">
+                   <div class="input-group">
+                       <input type="text" class="form-control" placeholder="Ширина">
+                       <input type="text" class="form-control" placeholder="Высота">
+                   </div>
 
+               </div>
+        </div>
+    </div>
+    <div class="col-xs-3">
+        <div class="panel panel-danger">
+            <!-- Default panel contents -->
+            <div class="panel-heading"><h4>3. Стеклопакет</h4></div>
+            <div class="panel-body">
+                <div class="input-group">
+                    <select class="form-control input-sm">
+                        <option value="">Одномерный</option>
+                        <option value="">Двухмерный</option>
+
+                    </select>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class=" col-xs-3">
+        <div class="panel panel-danger">
+            <!-- Default panel contents -->
+            <div class="panel-heading"><h4>4. Профиль</h4></div>
+            <div class="panel-body">
+                <div class="input-group">
+                    <select class="form-control input-sm">
+                        <option value="">KBE 58мм</option>
+                        <option value="">KBE 70мм</option>
+                        <option value="">Novotex 58мм</option>
+                        <option value="">Novotex 70мм</option>
+                        <option value="">Exprof 46мм</option>
+                        <option value="">Exprof 58мм</option>
+                        <option value="">Exprof 70мм</option>
+                    </select>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class=" col-xs-3">
+        <div class="panel panel-danger">
+            <!-- Default panel contents -->
+            <div class="panel-heading"><h4>5. Фурнитура</h4></div>
+            <div class="panel-body">
+                <div class="input-group">
+                    <select class="form-control input-sm">
+                        <option value="">Roto</option>
+                        <option value="">VHC</option>
+                    </select>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<div class="clear"></div>
+<div>
+
+    <div class=" col-xs-6">
+        <div class="panel panel-danger">
+            <!-- Default panel contents -->
+            <div class="panel-heading"><h4>5. Фурнитура</h4></div>
+            <div class="panel-body">
+                <div class="input-group">
+                    <label>
+                        <input type="checkbox" name="optionsRadios" id="optionsRadios2" value="option1" >
+                        <span> Глухое окно</span>
+
+                    </label>
+
+                    <label>
+                        <input type="checkbox" name="optionsRadios" id="optionsRadios3" value="option1" >
+                        <span>Арочное окно</span>
+
+                    </label>
+
+                    <label>
+                        <input type="checkbox" name="optionsRadios" id="optionsRadios4" value="option1" >
+                        <span>Арочное окно</span>
+                    </label>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class=" col-xs-6">
+        <div class="panel panel-danger">
+            <!-- Default panel contents -->
+            <div class="panel-heading"><h4>5. Фурнитура</h4></div>
+            <div class="panel-body">
+                <div class="input-group">
+                    <label>
+                        <input type="checkbox" name="optionsRadios" id="optionsRadios2" value="option1" >
+                        <span> Глухое окно</span>
+
+                    </label>
+
+                    <label>
+                        <input type="checkbox" name="optionsRadios" id="optionsRadios3" value="option1" >
+                        <span>Арочное окно</span>
+
+                    </label>
+
+                    <label>
+                        <input type="checkbox" name="optionsRadios" id="optionsRadios4" value="option1" >
+                        <span>Арочное окно</span>
+
+                    </label>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
 </div>

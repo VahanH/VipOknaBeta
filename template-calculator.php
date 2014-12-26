80b7a0f966f550ceb0f93d526e0af47544c9a977
Smart <petrosyan.vahe@rambler.ru>
2014-12-26 18:21:35 +0400

    calc page enter

--- template-calculator.php
+++ template-calculator.php
@@ -3,8 +3,59 @@
 Template Name: Calculator
 */
 ?>
+<div class="panel panel-danger">
+    <!-- Default panel contents -->
+    <div class="panel-heading"><h4>1. Тип конструкции</h4></div>
+    <div class="panel-body">
+        <div class="container">
+                    <label>
+                        <input type="radio" name="optionsRadios" id="optionsRadios2" value="option1" >
+                       <span> Глухое окно</span>
 
-<?php while (have_posts()) : the_post(); ?>
-    <?php get_template_part('templates/page', 'header'); ?>
-    <?php get_template_part('templates/content', 'page'); ?>
-<?php endwhile; ?>
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/gluxokno.png">
+                    </label>
+
+                    <label>
+                        <input type="radio"name="optionsRadios" id="optionsRadios3" value="option1" >
+                        <span>Арочное окно</span>
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/arochokno.png">
+
+                    </label>
+
+                    <label>
+                        <input type="radio" name="optionsRadios" id="optionsRadios4" value="option1" >
+                        <span>Арочное окно</span>
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/krugokno.png">
+
+
+                    </label>
+
+                    <label>
+                         <input type="radio" name="optionsRadios" id="optionsRadios5" value="option1" >
+                        <span>Двухстворчатое окно</span>
+
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/trexokno.png">
+                    </label>
+
+                    <label>
+                        <input type="radio" name="optionsRadios" id="optionsRadios6" value="option1" >
+                        <span>трехстворчатое окно</span>
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/garmoshka.png">
+                    </label>
+
+                    <label>
+                        <input type="radio" name="optionsRadios" id="optionsRadios7" value="option1" >
+                        <span>окно</span>
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/balkonokno.png">
+
+                    </label>
+
+                    <label>
+                       <input type="radio" name="optionsRadios" id="optionsRadios8" value="option1" >
+                           <span>окно</span>
+
+                        <img src="<?php bloginfo('template_url'); ?>/assets/img/dvuxokno.png">
+                    </label>
+        </div>
+
+</div>

1ec237fe2b7295456845a57e2285c68480ab21a5
VahanH <vahanius@gmail.com>
2014-12-28 17:07:18 +0400

    articles updated

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,8 +1,8 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
-    <div class="col-xs-12">
-      <div>
-        <h3><? the_title() ?></h3>
+    <div class="article col-xs-12">
+      <div class="">
+        <h3 class="articletitle"><? the_title() ?></h3>
         <div class="alignleft articleimg"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
         <div>
           <p><? the_content() ?></p>

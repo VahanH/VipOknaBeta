c638db15629ecb80e8706e3b7862552099597afa
VahanH <vahanius@gmail.com>
2014-12-28 16:50:43 +0400

    articles updated

--- template-articles.php
+++ template-articles.php
@@ -8,8 +8,8 @@ Template Name: Articles
     <?php $category_name=str_replace("/", "",get_the_title() ); $query = new WP_Query(array('category_name' => $category_name ));
     if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
         <div class="row articlenews">
-            <div class="col-sm-4 col-xs-12 thumbnail under-thumbnail-margin"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
-            <div class="col-sm-8 col-xs-12 caption">
+            <div class="col-sm-3 col-xs-12 thumbnail under-thumbnail-margin"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
+            <div class="col-sm-9 col-xs-12 caption">
                 <h3><? the_title() ?></h3>
                 <p><? the_content() ?></p>
                 <a href="<? the_permalink(); ?>" class="btn btn-default button-right button-margin">Подробнее</a>
--- templates/content-single.php
+++ templates/content-single.php
@@ -1,10 +1,10 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <div class="col-xs-12">
-      <div class="thumbnail">
-        <div class="thumb"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
-        <div class="caption">
-          <h3><? the_title() ?></h3>
+      <div>
+        <h3><? the_title() ?></h3>
+        <div class="alignleft articleimg"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
+        <div>
           <p><? the_content() ?></p>
         </div>
       </div>

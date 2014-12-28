19a436d6c7e3c82499935962cd3cc541588164d5
VahanH <vahanius@gmail.com>
2014-12-28 17:21:35 +0400

    articles updated

--- template-products.php
+++ template-products.php
@@ -29,8 +29,8 @@ Template Name: Products
 $first="active in";
 if ($query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();?>
         <div id="<?=get_the_ID() ?>" role="tabpanel" class="product_img tab-pane fade <?=$first;?>" aria-labelledby="<? the_title() ?>">
-           <div class="col-sm-4 col-xs-12 thumbnail"> <? echo get_the_post_thumbnail($id,'medium');?></div>
-            <div class="col-sm-8 col-xs-12"><p><? the_content() ?></p></div>
+           <div class="col-lg-3 col-sm-4 thumbnail"> <? echo get_the_post_thumbnail($id,'medium');?></div>
+            <div class="col-lg-9 col-sm-8"><p><? the_content() ?></p></div>
         </div>
 <?
 $first="";

ed0c03c2f0d56947b0354e1b16490cb58f908411
VahanH <vahanius@gmail.com>
2014-12-29 17:54:24 +0400

    owl carousel updated

--- template-home.php
+++ template-home.php
@@ -3,30 +3,46 @@
 Template Name: Home
 */
 ?>
-    <div id="owl-example" class="owl-carousel">
-        <div class="general-slider"><img class="" src="<?php bloginfo('template_url'); ?>/assets/img/slide1.jpg"></div>
-        <div class="general-slider"><img src="<?php bloginfo('template_url'); ?>/assets/img/retro_dra_69.jpg"></div>
+<div id="owl-example" class="owl-carousel">
+    <?php $query = new WP_Query(array('category_name' => 'slider'));
+    if ($query->have_posts()) : while ($query->have_posts()) :
+    $query->the_post(); ?>
+    <div class="general-slider">
+        <div class="sl-items">
+            <? echo get_the_post_thumbnail($id, 'full'); ?>
+        </div>
+            <h1><? the_title() ?></h1>
+            <p><? the_content() ?></p>
+
     </div>
+    <?
+    endwhile;
+    endif;
+    // Reset Query
+    //wp_reset_query();
+    ?>
+</div>
 <?php while (have_posts()) : the_post(); ?>
     <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
 
 <div class="row info">
-<?php $query = new WP_Query( array( 'category_name' => 'Info') );
-if ($query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();?>
-    <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-            <div class="thumb under-thumbnail-margin"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
-            <div class="caption">
-                <h3><? the_title() ?></h3>
-                <p><? the_content() ?></p>
+    <?php $query = new WP_Query(array('category_name' => 'Info'));
+    if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <div class="thumb under-thumbnail-margin"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
+                <div class="caption">
+                    <h3><? the_title() ?></h3>
+
+                    <p><? the_content() ?></p>
+                </div>
             </div>
         </div>
-    </div>
-<?
-endwhile;
-endif;
-// Reset Query
-//wp_reset_query();
-?>
+    <?
+    endwhile;
+    endif;
+    // Reset Query
+    //wp_reset_query();
+    ?>
 </div>

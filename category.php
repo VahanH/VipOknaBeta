b16bea035bb7b3d7c7b2a972e69b7bebaca3684a
Smart <petrosyan.vahe@rambler.ru>
2015-01-27 15:04:51 +0400

    category permalinks add

--- category.php
+++ category.php
@@ -7,9 +7,9 @@ Template Name: Articles
 <div>
     <?php  if (have_posts()) : while (have_posts()) : the_post(); ?>
         <div class="row articlenews">
-            <div class="col-lg-3 col-sm-4 thumbnail under-thumbnail-margin"><? echo get_the_post_thumbnail($id, 'medium'); ?></div>
+            <div class="col-lg-3 col-sm-4 thumbnail under-thumbnail-margin"><a href="<? the_permalink(); ?>"><? echo get_the_post_thumbnail($id, 'medium'); ?></a></div>
             <div class="col-lg-9 col-sm-8 caption">
-                <h3><? the_title() ?></h3>
+                <h3><a href="<? the_permalink(); ?>"><? the_title() ?></a></h3>
                 <p><? the_content() ?></p>
                 <a href="<? the_permalink(); ?>" class="btn btn-default button-right button-margin">Подробнее</a>
             </div>

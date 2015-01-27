1757cfce8904aea7862cdaec517106c44013c1c1
Smart <petrosyan.vahe@rambler.ru>
2015-01-27 13:12:23 +0400

    map is under txt

--- template-contact.php
+++ template-contact.php
@@ -5,12 +5,16 @@ Template Name: Contact
 ?>
 <div class="node-content">
     <div class="field field-name-body field-type-text-with-summary field-label-hidden">
-        <div class="field-item even" property="content:encoded">
-            <script type="text/javascript" charset="utf-8" src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=wkZjX6NT_PZUqK5VW0VszGtlmslcsUui&amp;width=100%&amp;height=300"></script>
+        <div class=" field-item even" property="content:encoded">
+            <div class=" margin-bottom row">
             <?php while (have_posts()) : the_post(); ?>
                            <?php get_template_part('templates/content', 'page'); ?>
             <?php endwhile; ?>
-
+            </div>
+            <div class="clear"></div>
+            <div class="button-margin">
+                <script type="text/javascript" charset="utf-8" src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=wkZjX6NT_PZUqK5VW0VszGtlmslcsUui&amp;width=100%&amp;height=300"></script>
+            </div>
         </div>
     </div>
 </div>
